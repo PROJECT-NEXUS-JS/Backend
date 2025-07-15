4c313fa7a33114558769ef7b1513232c65894d71
윤준석(YunJunSeok) <83647215+junseok0304@users.noreply.github.com>
2025-07-16 00:18:54 +0900

    Swagger UI에 “Authorize” 버튼을 추가 Authorization: Bearer <token> 방식의 JWT 인증 스펙 반영.

---
+++ src/main/java/com/example/nexus/app/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.example.nexus.app.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.ArrayList;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class SwaggerConfig {
+
+    public SwaggerConfig(MappingJackson2HttpMessageConverter converter) {
+        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
+        converter.setSupportedMediaTypes(supportedMediaTypes);
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "Authorization";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+        return new OpenAPI()
+                .components(components)
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement);
+    }
+    private Info apiInfo() {
+        return new Info()
+                .title("BETALAB BACKEND SERVER")
+                .description("베타랩 API swagger page 입니다.")
+                .version("1.0.0");
+    }
+}
